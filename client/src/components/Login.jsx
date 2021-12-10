172a9be6a0854dffb2f76b1dfa93c7debcc73666
maayan <maayan270485@gmail.com>
2021-12-10 21:55:20 +0200

    register repair

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -92,7 +92,7 @@ export default function Login() {
                         </Button>
                         <Grid container>
                             <Grid item>
-                                <Link href="users/register" variant="body2">
+                                <Link href="/register" variant="body2">
                                     {"Don't have an account? Sign Up"}
                                 </Link>
                             </Grid>
